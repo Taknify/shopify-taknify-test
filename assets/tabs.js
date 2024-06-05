ec89fd1baa84f064aed6e37ec09a9b24540e0cfc
Prasuna Dahal <prasunad@taknify.com>
2024-06-05 17:44:38 +0545

    mobile-design done

--- assets/tabs.js
+++ assets/tabs.js
@@ -1,25 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   const tabs = document.querySelectorAll(".tabs-container .tab");
 
-  // Function to deactivate all tabs
   function deactivateAllTabs() {
     tabs.forEach((otherTab) => {
+      const iconPlus = otherTab.querySelector(".plus-icon");
+      const iconMinus = otherTab.querySelector(".minus-icon");
       otherTab
         .querySelector(".tab-link")
         .setAttribute("aria-selected", "false");
       otherTab.querySelector(".tab-panel").setAttribute("hidden", true);
+      iconPlus.style.display = "inline";
+      iconMinus.style.display = "none";
     });
   }
 
-  // Function to activate a tab
   function activateTab(tab) {
     const tabLink = tab.querySelector(".tab-link");
     const tabPanel = tab.querySelector(".tab-panel");
+    const iconPlus = tab.querySelector(".plus-icon");
+    const iconMinus = tab.querySelector(".minus-icon");
     tabLink.setAttribute("aria-selected", "true");
     tabPanel.removeAttribute("hidden");
+    iconPlus.style.display = "none";
+    iconMinus.style.display = "inline";
   }
 
-  // Set up event listeners
   tabs.forEach((tab) => {
     const tabLink = tab.querySelector(".tab-link");
     tabLink.addEventListener("click", function (e) {
@@ -31,8 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Activate the first tab by default
   if (tabs.length > 0) {
+    deactivateAllTabs();
     activateTab(tabs[0]);
   }
 });
